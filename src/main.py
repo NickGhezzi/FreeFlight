a1d40ebfbeabfacf0fd85126ab369445a8d9d0d9
Nick Ghezzi <54958462+NickGhezzi@users.noreply.github.com>
2024-01-26 17:18:31 -0500

    Initial commit

---
+++ src/main.py
@@ -0,0 +1,44 @@
+import pygame, sys
+
+# Init
+pygame.init()
+#screen
+SCREEN_WIDTH = 600;
+SCREEN_HEIGHT = 600;
+screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
+#clock
+clock = pygame.time.Clock()
+    #CIRCLE
+#posx = SCREEN_WIDTH/2
+#posy =  SCREEN_HEIGHT/2
+
+posx = 0
+posy =  0
+
+
+# Main game loop
+while True:
+    # Close game
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    # Delta time
+    dt = clock.tick(60) / 1000
+
+    # Background
+    screen.fill((0, 0, 0))
+
+    gravity = -9.8
+
+
+    #CIRCLE
+    posy += gravity * dt 
+    # Draw a solid blue circle in the center
+    circle = pygame.draw.circle(screen, (0, 0, 255), (posx, posy), 75)
+
+    print(circle.center, dt)
+
+    # Flip the display
+    pygame.display.flip()
